7c580d39506d54781e07275591ea3a48f6e48675
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-08-10 21:21:50 +0200

    feat: base dir

--- src/configurations.rs
+++ src/configurations.rs
@@ -1,5 +1,24 @@
-use std::path::PathBuf;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 pub struct Configurations {
     pub node_modules_location: PathBuf,
+}
+
+impl Configurations {
+    fn retrieve_current_working_directory() -> Result::<String, env::VarError> {
+        Ok(env::current_dir().unwrap().to_str().unwrap().to_string())
+    }
+
+    pub fn new() -> Configurations {
+        let base_directory = env::var("BASE_DIRECTORY")
+            .or_else(|_| Configurations::retrieve_current_working_directory())
+            .unwrap();
+
+        Configurations {
+            node_modules_location: Path::new(&base_directory).join("node_modules"),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::{env, path::Path};
-
 use configurations::Configurations;
 
 mod cleaner;
 mod configurations;
 
 fn main() {
-    let configurations = Configurations {
-        node_modules_location: Path::new(&env::current_dir().unwrap()).join("node_modules"),
-    };
-
-    cleaner::clean(&configurations);
+    cleaner::clean(&Configurations::new());
 }
