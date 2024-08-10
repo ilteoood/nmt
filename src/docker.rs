f7b4797cddc5eb7bfee87d4a3f18380615041af8
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-08-11 00:17:32 +0200

    feat: docker with inputs

--- src/docker.rs
+++ src/docker.rs
@@ -1,24 +1,43 @@
 use bollard::image::BuildImageOptions;
+use bollard::secret::BuildInfo;
 use bollard::Docker;
 
 use futures_util::stream::StreamExt;
 
-use std::io::Write;
+use std::{fs::File, io::{Read, Write}};
 
-fn create_header(dockerfile: &str) -> tar::Header {
+fn create_header(name: &str, size: usize) -> tar::Header {
     let mut header = tar::Header::new_gnu();
-    header.set_path("Dockerfile").unwrap();
-    header.set_size(dockerfile.len() as u64);
+    header.set_path(name).unwrap();
+    header.set_size(size as u64);
     header.set_mode(0o755);
     header.set_cksum();
 
     header
 }
 
+fn create_dockerfile_header(dockerfile: &str) -> tar::Header {
+    create_header("Dockerfile", dockerfile.len())
+}
+
+fn create_cli_header(size: usize) -> tar::Header {
+    create_header("cli", size)
+}
+
+fn read_cli_file() -> Vec<u8> {
+    let mut f = File::open("./cli").unwrap();
+    let mut buffer = vec![];
+    f.read_to_end(&mut buffer).unwrap();
+
+    buffer
+}
+
 fn create_tar(dockerfile: &str) -> tar::Builder<Vec<u8>> {
-    let header = create_header(&dockerfile);
+    let cli_file = read_cli_file();
+
     let mut tar = tar::Builder::new(Vec::new());
-    tar.append(&header, dockerfile.as_bytes()).unwrap();
+    tar.append(&create_dockerfile_header(&dockerfile), dockerfile.as_bytes()).unwrap();
+    tar.append(&create_cli_header(cli_file.len()), &cli_file[..]).unwrap();
 
     tar
 }
@@ -36,24 +55,35 @@ fn create_compressed_tar(dockerfile: &str) -> Vec<u8> {
 fn create_dockerfile(source_image: &str) -> String {
     format!(
         "FROM {source_image}
-RUN touch buildkit-bollard.txt"
+COPY ./cli .
+RUN ls -lah
+RUN ./cli && rm -f ./cli"
     )
 }
 
-async fn run_build(build_image_options: BuildImageOptions<&str>, docker: &Docker, compressed_tar: Vec<u8>) {
+fn print_build_step(build_info: BuildInfo) {
+    let content = build_info
+        .status
+        .or_else(|| build_info.stream)
+        .or_else(|| Some(String::from("")))
+        .unwrap();
+
+    print!("{content}");
+}
+
+async fn run_build(
+    build_image_options: BuildImageOptions<&str>,
+    docker: &Docker,
+    compressed_tar: Vec<u8>,
+) {
     let mut image_build_stream =
         docker.build_image(build_image_options, None, Some(compressed_tar.into()));
 
     while let Some(msg) = image_build_stream.next().await {
-        let msg = msg.unwrap();
-
-        let content = msg
-            .status
-            .or_else(|| msg.stream)
-            .or_else(|| Some(String::from("")))
-            .unwrap();
-
-        print!("{content}");
+        match msg {
+            Ok(build_info) => print_build_step(build_info),
+            Err(err) => println!("Error while building image: {err}"),
+        }
     }
 }
 
@@ -61,7 +91,7 @@ async fn run_build(build_image_options: BuildImageOptions<&str>, docker: &Docker
 async fn main() {
     let docker = Docker::connect_with_socket_defaults().unwrap();
 
-    let dockerfile = create_dockerfile("alpine");
+    let dockerfile = create_dockerfile("debian");
 
     let compressed_tar = create_compressed_tar(&dockerfile);
 
@@ -69,7 +99,6 @@ async fn main() {
         dockerfile: "Dockerfile",
         nocache: true,
         rm: true,
-        squash: true,
         pull: true,
         ..Default::default()
     };
