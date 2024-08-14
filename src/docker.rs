95ea97e93f593aa2bbb137e542ede0bd3ce7b18d
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-08-14 16:57:08 +0200

    fix: history

--- src/docker.rs
+++ src/docker.rs
@@ -22,11 +22,8 @@ fn create_dockerfile_header(dockerfile: &str) -> tar::Header {
 
 fn create_tar(dockerfile: &str) -> tar::Builder<Vec<u8>> {
     let mut tar = tar::Builder::new(Vec::new());
-    tar.append(
-        &create_dockerfile_header(dockerfile),
-        dockerfile.as_bytes(),
-    )
-    .unwrap();
+    tar.append(&create_dockerfile_header(dockerfile), dockerfile.as_bytes())
+        .unwrap();
 
     tar
 }
@@ -41,13 +38,16 @@ fn create_compressed_tar(dockerfile: &str) -> Vec<u8> {
     c.finish().unwrap()
 }
 
-fn create_dockerfile(source_image: &str) -> String {
-    format!(
-        "FROM ilteoood/nmt as trimmer
-FROM {source_image}
-COPY --from=trimmer ./cli .
-RUN ./cli && rm -f ./cli"
-    )
+fn create_dockerfile(source_image: &str, history: History) -> String {
+    format!(r##"FROM ilteoood/nmt as nmt_trimmer
+    FROM {source_image} as source_image
+    COPY --from=nmt_trimmer ./cli .
+    RUN ./cli && rm -f ./cli
+    FROM scratch
+    COPY --from=source_image / /
+    {}
+    {}
+    {}"##, history.workdir, history.command, history.entry_point)
 }
 
 fn print_build_step(build_info: BuildInfo) {
@@ -75,14 +75,51 @@ async fn run_build(
     }
 }
 
+struct History {
+    workdir: String,
+    command: String,
+    entry_point: String,
+}
+
+async fn retrieve_history(
+    docker: &Docker,
+    source_image: &str,
+) -> Result<History, bollard::errors::Error> {
+    let history = docker.image_history(source_image).await?;
+
+    let mut entry_point: String = String::new();
+    let mut command = String::new();
+    let mut workdir = String::new();
+
+    for history_item in history {
+        match history_item.created_by.to_lowercase() {
+            entry if entry_point.is_empty() && entry.starts_with("entrypoint") => {
+                entry_point = entry.replace("[", "").replace("]", "")
+            }
+            cmd if command.is_empty() && cmd.starts_with("command") => command = cmd,
+            wd if workdir.is_empty() && wd.starts_with("workdir") => workdir = wd,
+            _ => {}
+        }
+    }
+
+    Ok(History {
+        workdir,
+        command,
+        entry_point,
+    })
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), bollard::errors::Error> {
     let docker = Docker::connect_with_socket_defaults().unwrap();
 
     let source_image = "ilteoood/xdcc-mule";
+
+    let history = retrieve_history(&docker, source_image).await?;
+
     let destination_image = format!("{source_image}:trimmed");
 
-    let dockerfile = create_dockerfile(source_image);
+    let dockerfile = create_dockerfile(source_image, history);
 
     let compressed_tar = create_compressed_tar(&dockerfile);
 
@@ -96,4 +133,6 @@ async fn main() {
     };
 
     run_build(build_image_options, &docker, compressed_tar).await;
+
+    Ok(())
 }
