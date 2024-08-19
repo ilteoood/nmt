010f92f983862eb247b7fe7f9c2fd2781a14e71c
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-08-19 22:49:47 +0200

    feat: minify

--- src/minifier.rs
+++ src/minifier.rs
@@ -14,22 +14,7 @@ fn retrieve_js_files(configurations: &CliConfigurations) -> Vec<PathBuf> {
     retrieve_glob_paths(vec![js_glob_path.to_string()])
 }
 
-fn compile(path: &PathBuf, cm: &Arc<SourceMap>, opts: &config::Options) -> String {
-    let c = swc::Compiler::new(cm.clone());
-    let output = GLOBALS
-        .set(&Default::default(), || {
-            try_with_handler(cm.clone(), Default::default(), |handler| {
-                let fm = cm.load_file(path.as_path()).expect("failed to load file");
-                Ok(c.process_js_file(fm, handler, &opts)
-                    .expect("failed to process file"))
-            })
-        })
-        .unwrap();
-
-    output.code
-}
-
-pub fn minify_js(configurations: &CliConfigurations) {
+fn build_compiler() -> impl Fn(&PathBuf) -> String {
     let cm = Arc::<SourceMap>::default();
 
     let opts = config::Options {
@@ -49,14 +34,101 @@ pub fn minify_js(configurations: &CliConfigurations) {
         ..Default::default()
     };
 
+    return move |path: &PathBuf| -> String {
+        let c = swc::Compiler::new(cm.clone());
+        let output = GLOBALS
+            .set(&Default::default(), || {
+                try_with_handler(cm.clone(), Default::default(), |handler| {
+                    let fm = cm.load_file(path.as_path()).expect("failed to load file");
+                    Ok(c.process_js_file(fm, handler, &opts)
+                        .expect("failed to process file"))
+                })
+            })
+            .unwrap();
+
+        output.code
+    };
+}
+
+pub fn minify_js(configurations: &CliConfigurations) {
     let to_compile = retrieve_js_files(configurations);
+    let compiler = build_compiler();
 
     for path in to_compile {
-        let code = compile(&path, &cm, &opts);
+        let code = compiler(&path);
 
         match fs::write(&path, code) {
             Ok(_) => println!("File minified: {}", path.display()),
             Err(error) => println!("Failed to write file {}: {}", path.display(), error),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests_retrieve_js_files {
+    use std::env;
+
+    use super::*;
+
+    fn retrieve_tests_ilteoood() -> PathBuf {
+        PathBuf::from(env::current_dir().unwrap())
+            .join("tests")
+            .join("node_modules")
+            .join("ilteoood")
+    }
+
+    #[test]
+    fn test_retrieve_js_files() {
+        let js_paths = retrieve_tests_ilteoood();
+
+        assert_eq!(
+            retrieve_js_files(&CliConfigurations {
+                node_modules_location: PathBuf::from(&js_paths),
+                ..Default::default()
+            }),
+            vec![
+                PathBuf::from(&js_paths).join("legit.esm.js"),
+                PathBuf::from(&js_paths).join("legit.js"),
+                PathBuf::from(&js_paths).join("unlegit.min.js"),
+            ]
+        );
+    }
+}
+
+#[cfg(test)]
+mod tests_compile {
+    use std::env;
+
+    use super::*;
+
+    fn retrieve_tests_ilteoood() -> PathBuf {
+        PathBuf::from(env::current_dir().unwrap())
+            .join("tests")
+            .join("node_modules")
+            .join("ilteoood")
+    }
+
+    #[test]
+    fn test_compile_esm() {
+        let js_path = retrieve_tests_ilteoood().join("legit.esm.js");
+
+        let compiler = build_compiler();
+
+        assert_eq!(
+            compiler(&js_path),
+            "import t from\"path\";export default function(e){return\".md\"===t.extname(e)}"
+        );
+    }
+
+    #[test]
+    fn test_compile_cjs() {
+        let js_path = retrieve_tests_ilteoood().join("legit.js");
+
+        let compiler = build_compiler();
+
+        assert_eq!(
+            compiler(&js_path),
+            "const e=require(\"path\");module.exports=function(t){return\".md\"===e.extname(t)};"
+        );
     }
 }
