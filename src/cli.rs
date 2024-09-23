810bc08d098a96505c60a4d6dae36ff1ca60d179
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-09-23 09:07:45 +0200

    feat: new config

--- src/cli.rs
+++ src/cli.rs
@@ -1,21 +1,23 @@
-use nmt::{cleaner, configurations::CliConfigurations, minifier};
+// use nmt::{cleaner, configurations::CliConfigurations, minifier};
+use nmt::configurations::CliConfigurations;
 
 fn main() {
     let configurations = &CliConfigurations::new();
 
-    let garbage_paths = cleaner::retrieve_garbage(configurations);
+    // let garbage_paths = cleaner::retrieve_garbage(configurations);
 
     if !configurations.dry_run {
-        cleaner::clean(configurations, garbage_paths);
+        // cleaner::clean(configurations, garbage_paths);
     } else {
         println!("Dry run. These are the paths that should be removed:");
-        garbage_paths
+        /*garbage_paths
             .iter()
             .for_each(|path| println!("{}", path.display()));
+        */
     }
 
     if configurations.minify {
-        minifier::minify_js(configurations);
+        // minifier::minify_js(configurations);
     } else {
         println!("Minification skipped");
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-pub mod cleaner;
+// pub mod cleaner;
 pub mod configurations;
 pub mod container_configurations;
 pub mod glob;
-pub mod minifier;
+// pub mod minifier;
 pub mod module_graph;
