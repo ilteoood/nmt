8d3a802c08e3961fb5d346705f833c72a33857fe
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2026-08-08 20:02:39 +0200

    test: bump pinned fixture to current xdcc-mule image
    The upstream `ilteoood/xdcc-mule:latest` image was rebuilt since the
    last fixture update: entry point is now `index.mjs` and NODE_VERSION
    is 24.19.0. Refresh the expected values so CI matches reality.

--- src/docker.rs
+++ src/docker.rs
@@ -204,10 +204,10 @@ mod history_tests {
             ContainerConfigurations {
                 workdir: Some(String::from("WORKDIR /app")),
                 command: None,
-                entry_point: Some(String::from("ENTRYPOINT [\"node\", \"index.js\"]")),
+                entry_point: Some(String::from("ENTRYPOINT [\"node\", \"index.mjs\"]")),
                 health_check: None,
                 user: None,
-                env: Some(String::from("ENV PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\nENV NODE_VERSION=24.18.0\nENV YARN_VERSION=1.22.22")),
+                env: Some(String::from("ENV PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\nENV NODE_VERSION=24.19.0\nENV YARN_VERSION=1.22.22")),
             }
         );
     }
